d0735a2f90b511b160101e6dda62f486108dc45d
chihebchabou <chihebchaboutech@gmail.com>
2024-10-12 13:44:56 +0100

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/shareposts/');
+
+// Site Name
+define('SITENAME', 'SharePosts');
+
+// App version
+define('APPVERSION', '1.0.0');
+
---
+++ app/views/pages/about.php
@@ -0,0 +1,5 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h1><?= $data['title'] ?></h1>
+<p class="lead"><?= $data['description'] ?></p>
+<p><strong>Version: <?= APPVERSION ?></strong></p>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
